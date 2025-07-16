2f0b6f88e19fb281231d0a8118d2e47b86f791b8
sahil <sahilchess09@gmail.com>
2025-07-16 12:12:45 -0400

    made killzone and enemy and made a background
    oops

---
+++ scripts/green_slime.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+const SPEED = 60
+var direction = 1
+
+@onready var ray_cast_left: RayCast2D = $RayCast_Left
+@onready var ray_cast_right: RayCast2D = $RayCast_right
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		direction=1
+		animated_sprite.flip_h = false
+	elif ray_cast_right.is_colliding():
+		direction=-1
+		animated_sprite.flip_h = true
+	else:
+		pass
+	position.x += SPEED * delta * direction
