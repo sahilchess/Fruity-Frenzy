bb55a0113ba4ff4a84a845fd6801aa5f4b310076
sahil <sahilchess09@gmail.com>
2025-07-17 13:04:04 -0400

    yay 10 hours!
    i was too locked in.. sorry forposting a big update

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,10 @@ const JUMP_VELOCITY = -300.0
 
 
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,9 +19,25 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction -1,0,1
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	# turn the character
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	if is_on_floor():
+		# play the run animation	
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+		
 	if direction	:
 		velocity.x = direction * SPEED
 	else:
