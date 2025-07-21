f9e3a9a95bc8f65187222fb3b0a1f185222b243f
sahil <sahilchess09@gmail.com>
2025-07-21 17:48:02 -0400

    level switcher failure

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -1,4 +1,5 @@
-extends Area2D
+extends Node2D
+@onready var next_level: Area2D = $next_level
 
 
 const FILE_BEGIN = "res://levels/level_"
@@ -9,4 +10,3 @@ func _on_body_entered(body: Node2D) -> void:
 	var next_level_number = current_scene_file.to_int() + 1
 		
 	var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
-	get_tree().change_scene_to_file(next_level_path)
