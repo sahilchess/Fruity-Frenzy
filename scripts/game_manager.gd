6620dbf6c7f754859c53597690d9e7a3fce582fc
sahil <sahilchess09@gmail.com>
2025-07-21 22:05:52 -0400

    fine tuned level switching
    nxt up: try to make coins work

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,5 +5,5 @@ var score = 0
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
-	score += 1 
-	score_label.text = "You collected %s coins" % [str(score)]
+	score += 1
+	print( "You collected " + str(score) + " coins" )
