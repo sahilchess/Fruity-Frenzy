1dee5aa4db45559350219e527b067410a2600954
sahil <sahilchess09@gmail.com>
2025-07-24 10:37:23 -0400

    Refactor peach_jump scene and script structure

--- scripts/next_level.gd
+++ scripts/next_level.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 const FILE_BEGIN = "res://levels/level_"
 
-func _on_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1
--- scripts/peach_jump.gd
+++ scripts/peach_jump.gd
@@ -1,22 +1,15 @@
-extends Node2D
+extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var powerup: AudioStreamPlayer = $powerup
-@onready var area_2d: Area2D = $Area2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $Area2D/AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
-
-# 130
-# -300
-
-# 200
-# -500
-
-# slowness is 50
+@onready var level_up: AudioStreamPlayer = $level_up
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	collision_shape_2d.disabled = true
+func _on_body_entered(body: Node2D) -> void:
+	print("hihihiih")
 	animated_sprite_2d.hide()
-	powerup.play()
-	await powerup.finished
+	collision_shape_2d.disabled= true
+	level_up.play()
+	await level_up.finished
 	queue_free()
